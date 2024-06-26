e82174daa5c2d2ceefaafc46787da94b1907e49b
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-06-26 15:35:56 +0330

    localStorage Todo: add changeStatusHandler

--- pages/intermediate-level/11-localStorage-todoList/js/app.js
+++ pages/intermediate-level/11-localStorage-todoList/js/app.js
@@ -30,8 +30,8 @@ function createTodoItems(){
             `
             <label>${todo.text}</label>
             <div class="todoList_item__btn">
-                <button class="btn btn-success" onclick="changeStatusHandler(event)">Complete</button>
-                <button class="btn btn-danger">Delete</button>
+                <button class="btn btn-success status-btn " onclick="changeStatusHandler(event)">${todo.status}</button>
+                <button class="btn btn-danger delete-btn">Delete</button>
             </div>
           
         `
@@ -41,16 +41,17 @@ function createTodoItems(){
 }
 
     function changeStatusHandler(event) {
-        console.log("event: ", event.target.parentElement.parentElement);
         let targetElm = event.target.parentElement.parentElement
         if (targetElm.classList.contains("completed")){
             targetElm.classList.remove("completed")
             targetElm.classList.add("uncompleted")
+            event.target.innerHTML = "uncompleted"
         }else{
             targetElm.classList.remove("uncompleted")
             targetElm.classList.add("completed")
+            event.target.innerHTML = "completed"
+
         }
-        
 }
 
 
