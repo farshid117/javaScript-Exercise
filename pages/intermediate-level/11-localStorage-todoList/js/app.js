f3aff1e7064f8849f2d3dac67d85f368e5be6eda
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-06-26 16:45:57 +0330

    localStorage Todolist : add deleteTodoHandler

--- pages/intermediate-level/11-localStorage-todoList/js/app.js
+++ pages/intermediate-level/11-localStorage-todoList/js/app.js
@@ -30,8 +30,8 @@ function createTodoItems(){
             `
             <label>${todo.text}</label>
             <div class="todoList_item__btn">
-                <button class="btn btn-success status-btn " onclick="changeStatusHandler(event)">${todo.status}</button>
-                <button class="btn btn-danger delete-btn">Delete</button>
+                <button class="btn btn-success status-btn " onclick="changeStatusHandler(event, ${todo.id})">${todo.status}</button>
+                <button class="btn btn-danger delete-btn" onclick="deleteTodoHandler(event, ${todo.id})">Delete</button>
             </div>
           
         `
@@ -40,18 +40,46 @@ function createTodoItems(){
     })
 }
 
-    function changeStatusHandler(event) {
+    function changeStatusHandler(event,id) {
+
+        let array = JSON.parse(localStorage.getItem("LocalStorageTodosArray"))
+        console.log("array: ", array);
         let targetElm = event.target.parentElement.parentElement
         if (targetElm.classList.contains("completed")){
             targetElm.classList.remove("completed")
             targetElm.classList.add("uncompleted")
             event.target.innerHTML = "uncompleted"
+
+            let todoIndex = array.findIndex(todo => todo.id === id)
+            let todo = array.find(todo => todo.id === id)
+            todo.status = "uncompleted"
+
+            array[todoIndex] = todo
+
+            localStorage.setItem("LocalStorageTodosArray", JSON.stringify(array))
         }else{
             targetElm.classList.remove("uncompleted")
             targetElm.classList.add("completed")
             event.target.innerHTML = "completed"
 
+            let todoIndex = array.findIndex(todo => todo.id === id)
+            let todo = array.find(todo => todo.id === id)
+            todo.status = "completed"
+
+            array[todoIndex] = todo
+
+            localStorage.setItem("LocalStorageTodosArray",JSON.stringify(array))
+
         }
+}
+function deleteTodoHandler(event, id) {
+    event.target.parentElement.parentElement.remove()
+    let array = JSON.parse(localStorage.getItem("LocalStorageTodosArray"))
+
+    let todoIndex = array.findIndex(todo => todo.id === id)
+    array.splice(todoIndex, 1)
+    localStorage.setItem("LocalStorageTodosArray", JSON.stringify(array))
+
 }
 
 
