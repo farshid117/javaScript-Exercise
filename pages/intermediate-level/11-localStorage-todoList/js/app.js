8260c75c8c0aec9667cdc3d719fd17b36938692d
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-06-27 10:58:05 +0330

    LocalStorage TodoList: do some clearCode

--- pages/intermediate-level/11-localStorage-todoList/js/app.js
+++ pages/intermediate-level/11-localStorage-todoList/js/app.js
@@ -1,26 +1,37 @@
 const $ = document
-const itemInput = $.querySelector("#itemInput") 
-const UlTodoList = $.querySelector("#todoList") 
+const itemInput = $.querySelector("#itemInput")
+const UlTodoList = $.querySelector("#todoList")
 const addButton = $.querySelector("#addButton")
 const clearButton = $.querySelector("#clearButton")
 
-let todosArray = []
-let count = 0
-addButton.addEventListener("click",() => {
-    
-    const todo = {
-        id: ++count,
-        text: itemInput.value.trim(),
-        status: "uncompleted",
-    }
-    todosArray.push(todo)
-    console.log("todosArray: ", todosArray);
-    localStorage.setItem("LocalStorageTodosArray", JSON.stringify(todosArray))
 
-   createTodoItems()
+let count = 0
+let todosArray = []
+
+addButton.addEventListener("click", () => {
+
+    if (itemInput.value.trim()) {
+        const todo = {
+            id: ++count,
+            text: itemInput.value.trim(),
+            status: "uncompleted",
+        }
+
+        todosArray.push(todo)
+        console.log("todosArray: ", todosArray);
+
+        setLocalStorage(todosArray)
+        createTodoItems()
+
+    } else {
+        alert("آقای خان مقداری را وارد کن!!")
+    }
 })
 
-function createTodoItems(){
+function setLocalStorage(todosList) {
+    localStorage.setItem("LocalStorageTodosArray", JSON.stringify(todosList))
+}
+function createTodoItems() {
     UlTodoList.innerHTML = ""
     let LocalStorageTodosArray = JSON.parse(localStorage.getItem("LocalStorageTodosArray"))
     LocalStorageTodosArray.forEach((todo) => {
@@ -30,7 +41,7 @@ function createTodoItems(){
             `
             <label>${todo.text}</label>
             <div class="todoList_item__btn">
-                <button class="btn btn-success status-btn " onclick="changeStatusHandler(event, ${todo.id})">${todo.status}</button>
+                <button class="btn btn-success status-btn " onclick="changeStatusHandler(event, ${todo.id})">${todo.status === "completed" ? "uncompleted" : "completed"}</button>
                 <button class="btn btn-danger delete-btn" onclick="deleteTodoHandler(event, ${todo.id})">Delete</button>
             </div>
           
@@ -39,38 +50,39 @@ function createTodoItems(){
         itemInput.value = ""
     })
 }
+function changeStatusHandler(event, id) {
 
-    function changeStatusHandler(event,id) {
+    let array = JSON.parse(localStorage.getItem("LocalStorageTodosArray"))
+    console.log("array: ", array);
+    let targetElm = event.target.parentElement.parentElement
+    if (targetElm.classList.contains("completed")) {
+        targetElm.classList.remove("completed")
+        targetElm.classList.add("uncompleted")
+        event.target.innerHTML = "completed"
 
-        let array = JSON.parse(localStorage.getItem("LocalStorageTodosArray"))
-        console.log("array: ", array);
-        let targetElm = event.target.parentElement.parentElement
-        if (targetElm.classList.contains("completed")){
-            targetElm.classList.remove("completed")
-            targetElm.classList.add("uncompleted")
-            event.target.innerHTML = "uncompleted"
+        let todoIndex = array.findIndex(todo => todo.id === id)
+        let todo = array.find(todo => todo.id === id)
+        todo.status = "uncompleted"
 
-            let todoIndex = array.findIndex(todo => todo.id === id)
-            let todo = array.find(todo => todo.id === id)
-            todo.status = "uncompleted"
+        array[todoIndex] = todo
+        console.log("array1: ", array);
 
-            array[todoIndex] = todo
+        localStorage.setItem("LocalStorageTodosArray", JSON.stringify(array))
+    } else {
+        targetElm.classList.remove("uncompleted")
+        targetElm.classList.add("completed")
+        event.target.innerHTML = "uncompleted"
 
-            localStorage.setItem("LocalStorageTodosArray", JSON.stringify(array))
-        }else{
-            targetElm.classList.remove("uncompleted")
-            targetElm.classList.add("completed")
-            event.target.innerHTML = "completed"
+        let todoIndex = array.findIndex(todo => todo.id === id)
+        let todo = array.find(todo => todo.id === id)
+        todo.status = "completed"
 
-            let todoIndex = array.findIndex(todo => todo.id === id)
-            let todo = array.find(todo => todo.id === id)
-            todo.status = "completed"
+        array[todoIndex] = todo
+        console.log("array2: ", array);
 
-            array[todoIndex] = todo
+        localStorage.setItem("LocalStorageTodosArray", JSON.stringify(array))
 
-            localStorage.setItem("LocalStorageTodosArray",JSON.stringify(array))
-
-        }
+    }
 }
 function deleteTodoHandler(event, id) {
     event.target.parentElement.parentElement.remove()
@@ -80,13 +92,13 @@ function deleteTodoHandler(event, id) {
     array.splice(todoIndex, 1)
     localStorage.setItem("LocalStorageTodosArray", JSON.stringify(array))
 
+    console.log(JSON.parse(localStorage.getItem("LocalStorageTodosArray")))
+
 }
 
 
-
-
-
-window.onload = () =>{
+window.onload = () => {
     itemInput.focus()
     createTodoItems()
+    console.log(JSON.parse(localStorage.getItem("LocalStorageTodosArray")))
 }
