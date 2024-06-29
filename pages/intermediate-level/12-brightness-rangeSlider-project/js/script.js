4ec54c4c70b1db60006c010d5fa9efed92bebfc1
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-06-29 17:01:16 +0330

    screen Brightness Slider: add javascript code for it

---
+++ pages/intermediate-level/12-brightness-rangeSlider-project/js/script.js
@@ -0,0 +1,6 @@
+const bgImg = document.querySelector(".container")
+const inputRange = document.querySelector("#range")
+
+inputRange.addEventListener("change",(event) => {
+    console.log(event.target.value)
+})
