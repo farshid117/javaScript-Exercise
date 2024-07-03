700f4274ccd543fb41c2c380c2f2bdb5a67c9882
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-07-03 12:14:17 +0330

    screen Brightness Slider: some adjust

--- pages/intermediate-level/12-brightness-rangeSlider-project/js/script.js
+++ pages/intermediate-level/12-brightness-rangeSlider-project/js/script.js
@@ -3,4 +3,7 @@ const inputRange = document.querySelector("#range")
 
 inputRange.addEventListener("change",(event) => {
     console.log(event.target.value)
+    bgImg.style.filter = `brightness(${event.target.value}%)`
+
 })
+
