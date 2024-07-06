057682cbb0115e40497cefa55321682bb64c6848
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-07-06 12:50:34 +0330

    javascript-Exercise --> Dynamic Change Theme pro: some adjustment js of project

--- pages/intermediate-level/15-dynamic-change-theme/js/script.js
+++ pages/intermediate-level/15-dynamic-change-theme/js/script.js
@@ -1,10 +1,10 @@
-const colorBtns = document.querySelectorAll('.btn')
+const colorBtns = document.querySelectorAll('.btn')//NodeList
 
-colorBtns.forEach(function (colorBtn) {
-
-    colorBtn.addEventListener('click', function (event) {
-        let btnColor = event.target.dataset.color
-
-        document.documentElement.style.setProperty('--theme-color', btnColor)
+colorBtns.forEach(colorBtn => {
+    colorBtn.addEventListener('click',(event) => {
+        let selectedColor = event.target.dataset.color
+        document.documentElement.style.setProperty('--theme-color', selectedColor)
+        console.log(document.documentElement.style.getPropertyValue('--theme-color'))
     })
 })
+
