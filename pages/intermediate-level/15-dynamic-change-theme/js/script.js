6d0af44528b05c40300ab7602c59ec5e609965ee
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-07-06 13:39:00 +0330

    javascript-Exercise --> animate virtual keyboardpro: add source of HTML/CSS

--- pages/intermediate-level/15-dynamic-change-theme/js/script.js
+++ pages/intermediate-level/15-dynamic-change-theme/js/script.js
@@ -3,6 +3,7 @@ const colorBtns = document.querySelectorAll('.btn')//NodeList
 colorBtns.forEach(colorBtn => {
     colorBtn.addEventListener('click',(event) => {
         let selectedColor = event.target.dataset.color
+        //Access to Css Variable in Js
         document.documentElement.style.setProperty('--theme-color', selectedColor)
         console.log(document.documentElement.style.getPropertyValue('--theme-color'))
     })
