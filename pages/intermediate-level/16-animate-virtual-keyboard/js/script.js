1b5dc3071166a2776898027069d029511f8cbf4e
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-07-07 15:06:44 +0330

    javascript-Exercise --> animate virtual keyboardpro: add source of javascript

--- pages/intermediate-level/16-animate-virtual-keyboard/js/script.js
+++ pages/intermediate-level/16-animate-virtual-keyboard/js/script.js
@@ -1,9 +1,55 @@
 const titleElem = document.querySelector('.title')
 
-document.addEventListener('keyup', function (event) {
+document.addEventListener("keyup", (event)=>{
+    let userSelectedkey = event.code
+    let virtualUserSelectedElm = document.getElementById(userSelectedkey)
+
+    appendUserSlectedKeyTODom(event, virtualUserSelectedElm)
+    
+})
+
+function appendUserSlectedKeyTODom(event, virtualUserSelectedElm) {
+    console.log("event: ", event);
+    if (event.code === "Space") {
+        titleElem.innerHTML += " "
+        return
+    } else if (event.code === "Backspace") {
+        titleElem.innerHTML = titleElem.innerHTML.slice(0, -1)
+        return
+    }
+    else {
+        titleElem.innerHTML += virtualUserSelectedElm.innerHTML
+        virtualUserSelectedElm.classList.add("hit")
+    }
+
+    virtualUserSelectedElm.addEventListener("animationend", () => {
+        virtualUserSelectedElm.classList.remove("hit")
+    })
+}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* document.addEventListener('keyup', function (event) {
     appendValueToDom(event)
 
     let userEventKey = event.key.toUpperCase() // 's a d'
@@ -18,15 +64,12 @@ document.addEventListener('keyup', function (event) {
     console.log(mainKeyElem);
 })
 
-
 function appendValueToDom (event) {
     console.log(event);
-
     if (event.code === 'Backspace') {
         titleElem.innerHTML = titleElem.innerHTML.slice(0, -1)
         return
     }
-
     titleElem.innerHTML += event.key
-
 }
+ */
