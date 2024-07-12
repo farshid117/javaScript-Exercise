b0e386541dbd46f2188686357b9b560f6c87c4a4
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-07-12 10:52:34 +0330

    javascript-Exercise --> pagination project: some refactor for cleanCode

--- pages/intermediate-level/20-pagnation-project/js/app.js
+++ pages/intermediate-level/20-pagnation-project/js/app.js
@@ -1,93 +1,74 @@
-const listItems = [
-    { id: 1, name: 'farshid', family: 'khayatpur' },
-    { id: 2, name: 'afshin', family: 'khayatpur' },
-    { id: 3, name: 'abbas', family: 'khayatpur' },
-    { id: 4, name: 'simin', family: 'smaeili' },
-    { id: 5, name: 'zahra', family: 'mirzaei' },
+const usersList = [
+    { id: 1, name: 'alireza', family: 'khaypour' },
+    { id: 2, name: 'abbas',   family: 'khayatpur' },
+    { id: 3, name: 'simin',   family: 'esmaeili' },
+    { id: 4, name: 'maryam',  family: 'khayatpour' },
+    { id: 5, name: 'farima',  family: 'khayatpour' },
 
-    { id: 6, name: 'maryam', family: 'khayapur' },
-    { id: 7, name: 'farima', family: 'khayatpour' },
-    { id: 8, name: 'rayan', family: 'esmaeili' },
+    { id: 6, name: 'afshin', family: 'khayatpour' },
+    { id: 7, name: 'zahra',  family: 'mirzaei' },
+    { id: 8, name: 'rayan',  family: 'khayatpour' },
     { id: 9, name: 'mohsen', family: 'pahlavan' },
-    { id: 10, name: 'mamad', family: 'seyedi' },
+    { id: 10, name: 'seyedi',family: 'mohamad' },
 
-    { id: 11, name: 'Saeed', family: 'Ehsani' },
+    { id: 11, name: 'Saeed',  family: 'Ehsani' },
     { id: 12, name: 'Siamak', family: 'Modiri' },
     { id: 13, name: 'Mohsen', family: 'Ansari' },
     { id: 14, name: 'Mehran', family: 'Ali Pour' },
-    { id: 15, name: 'Amir Hossein', family: 'Mahtabi' },
+    { id: 15, name: 'Amir ',  family: 'Mahtabi' },
 
     { id: 16, name: 'Hossein', family: 'Amino' },
-    { id: 17, name: 'Melika', family: 'Ehsani' },
-    { id: 18, name: 'Qadir', family: 'Yolme' },
+    { id: 17, name: 'Melika',  family: 'Ehsani' },
+    { id: 18, name: 'Qadir',   family: 'Yolme' },
     { id: 19, name: 'Fatemeh', family: 'Alilou' },
-    { id: 20, name: 'Ehsan', family: 'Tayyebi' },
+    { id: 20, name: 'Ehsan',   family: 'Tayyebi' },
 
     { id: 21, name: 'Zahra', family: 'Gholami' },
     { id: 22, name: 'Matin', family: 'Sahebi' },
-    
 ];
 
-let userListContainer = document.querySelector('#list')
+let userListContainer   = document.querySelector('#list')
 let paginationContainer = document.querySelector('#pagination')
 
-let currentPage = 1
-let rowsCount = 5
+let pageNumber = 1
+let pageSize = 4
 
-function displayUesrsList (allUesrsArray, usersContainer, rowsCount, currentPage) {
-    usersContainer.innerHTML = ''
+function displayUesrsList(targetElm) {
+    console.log("targetElm: ", targetElm);
+    if (targetElm) {
+        pageNumber = targetElm.innerHTML
+        userListGenerator()
 
-    let endIndex = rowsCount * currentPage
-    let startIndex = endIndex - rowsCount
+    } else {
+        pageNumber = 1
+        userListGenerator()
 
-    let paginatedUsers = allUesrsArray.slice(startIndex, endIndex)
-
-    paginatedUsers.forEach(function (user) {
-        let userElement = document.createElement('div')
-        userElement.classList.add('item')
-        userElement.innerHTML = user.name + ' ' + user.family
-
-        usersContainer.appendChild(userElement)
-    })
-
-}
-
-function setupPagination (allUesrsArray, pagesContainer, rowsCount) {
-    // Codes
-
-    pagesContainer.innerHTML = ''
-
-    let pageCount = Math.ceil(allUesrsArray.length / rowsCount)
-
-    for (let i = 1 ; i < pageCount + 1 ; i++) {
-        let btn = paginationButtonGenerator(i, allUesrsArray)
-        pagesContainer.appendChild(btn)
     }
-
 }
 
-function paginationButtonGenerator (page, allUesrsArray) {
-    let button = document.createElement('button')
-    button.innerHTML = page
+function userListGenerator() {
+    userListContainer.innerHTML = ""
+    let sliceArray = usersList.slice((pageNumber - 1) * pageSize, pageNumber * pageSize)
 
-    if (page === currentPage) {
-        button.classList.add('active')
+    sliceArray.forEach(item => {
+        userListContainer.innerHTML +=
+            `
+                <div class="item fw-bold">${item.name} ${item.family}</div>
+            `
+    });
+}
+
+function setupPagination() {
+    let numberBtn = Math.ceil(usersList.length / pageSize)
+    for (let i = 0; i < numberBtn; i++) {
+        paginationContainer.innerHTML +=
+         `
+            <button onclick="displayUesrsList(this)">${i + 1}</button>
+         `
     }
-
-    button.addEventListener('click', function () {
-        currentPage = page
-        displayUesrsList(allUesrsArray, userListContainer, rowsCount, currentPage)
-
-        let prevPage = document.querySelector('button.active')
-        prevPage.classList.remove('active')
-
-        button.classList.add('active')
-
-
-    })
-
-    return button
 }
 
-displayUesrsList(listItems, userListContainer, rowsCount, currentPage)
-setupPagination(listItems, paginationContainer, rowsCount)
+window.onload = () => {
+    displayUesrsList()
+    setupPagination()
+}
