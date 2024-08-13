2f58d24e8c357e390dcb15fe2d043a5908695aeb
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-08-13 21:18:42 +0330

    js-intermediate --> MiniProjects -> shoes-shopping Project : add some featuer

--- pages/intermediate-level/23-shoes-shopping-project/js/product.js
+++ pages/intermediate-level/23-shoes-shopping-project/js/product.js
@@ -1,4 +1,3 @@
-
 let $ = document
 let backBtn = $.querySelector('#back')
 let shoeTitle = $.querySelector('h1')
@@ -8,26 +7,25 @@ let shoeImage = $.querySelector('img')
 let productsArray = [
     { id: 1, title: 'Sport Shoe', price: 53, img: 'images/1.png', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atnobis, ipsum quas excepturi sed minus modi officia corrupti, veniamrem tempora, tenetur dicta nesciunt neque! Voluptate consequunturincidunt molestias? (Sport Shoe)' },
     { id: 2, title: 'Women Shoe', price: 81, img: 'images/2.png', desc: 'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Aperiam atnobis, ipsum quas excepturi sed minus modi officia corrupti, veniamrem tempora, tenetur dicta nesciunt neque! Voluptate consequunturincidunt molestias? (Women Shoe)' },
-    { id: 3, title: 'Boots',      price: 34, img: 'images/3.png',      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atnobis, ipsum quas excepturi sed minus modi officia corrupti, veniamrem tempora, tenetur dicta nesciunt neque! Voluptate consequunturincidunt molestias? (Boots)' },
+    { id: 3, title: 'Boots',      price: 34, img: 'images/3.png', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atnobis, ipsum quas excepturi sed minus modi officia corrupti, veniamrem tempora, tenetur dicta nesciunt neque! Voluptate consequunturincidunt molestias? (Boots)' },
 ]
 
 let locationParams = new URLSearchParams(location.search)
-let mainProductID = locationParams.get('id')
+let productID = locationParams.get('id') // string  
 
-let mainProductObject = productsArray.find((product) => product.id === Number(mainProductID) )
+let productObject = productsArray.find((product) => product.id === Number(productID) )
 
-if (mainProductObject) {
-    shoeTitle.innerHTML = mainProductObject.title
-    shoeDesc.innerHTML = mainProductObject.desc
-    shoeImage.setAttribute('src', mainProductObject.img)
+if (productObject) {
+    shoeTitle.innerHTML = productObject.title
+    shoeDesc.innerHTML = productObject.desc
+    // shoeImage.setAttribute('src', productObject.img)
+    shoeImage.src = productObject.img
 } else {
-    location.href = 'http://localhost/sabzlearn-js/247-coding-of-shoes-shop-project-2/source/'
+    history.back()
 }
 
 backBtn.addEventListener('click', function () {
     history.back()
 })
-
-
 
 // ✌ :))
