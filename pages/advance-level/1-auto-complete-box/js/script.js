6f6cbe43b522c1414f9bedc34f45f4ccf5c6b441
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-08-17 13:13:46 +0330

    js-Exersice -> js-advance -> auto Complete project : add js code

--- pages/advance-level/1-auto-complete-box/js/script.js
+++ pages/advance-level/1-auto-complete-box/js/script.js
@@ -4,14 +4,51 @@ let searchInpueElem = $.querySelector('input')
 let autoCompleteBox = $.querySelector('.autocom-box')
 
 searchInpueElem.addEventListener('keyup', function () {
-    let searchValue = searchInpueElem.value
+    if(searchInpueElem.value.trim()) {
+        autoCompleteWrapper.classList.add("active")
+        let wordsArray = suggestions.filter((word) => {
+            return word.toLowerCase().includes(searchInpueElem.value.toLowerCase())
+            // return word.toLowerCase().startsWith(searchInpueElem.value)
+        })
+        suggestionWordsGenerator(wordsArray)
+        // console.log("wordsArray: ", wordsArray);
+    }else{
+        autoCompleteWrapper.classList.remove("active")
+    }
+})
+
+function suggestionWordsGenerator(wordsArray) {
+    let listItemsArray= wordsArray.map(word => `<li>${word}</li>`)
+    // console.log("listItemsArray: ", listItemsArray);
+    
+    if(!listItemsArray.length){
+        autoCompleteBox.innerHTML = `<li>${searchInpueElem.value}</li>`
+    }else{
+        autoCompleteBox.innerHTML = listItemsArray.join("")
+        select()
+    }
+}
+
+function select () {
+    let allListItems = autoCompleteBox.querySelectorAll('li') //NodeList
+    // console.log("allListItems: ", allListItems);
+    allListItems.forEach(li => {
+        li.addEventListener("click",(event)=>{
+            searchInpueElem.value = event.target.innerText
+            autoCompleteWrapper.classList.remove("active")
+        })
+    } )
+} 
+/*
+searchInpueElem.addEventListener('keyup', function () {
+    let searchValue = searchInpueElem.value.trim()
 
     if (searchValue) {
         autoCompleteWrapper.classList.add('active')
 
         let filteredWords = suggestions.filter(function (word) {
-            return word.toLowerCase().startsWith(searchValue.toLowerCase())
-            // return word.toLowerCase().includes(searchValue.toLowerCase())
+            // return word.toLowerCase().startsWith(searchValue.toLowerCase())
+            return word.toLowerCase().includes(searchValue.toLowerCase())
         })
 
         suggestionWordsGenerator(filteredWords)
@@ -38,7 +75,7 @@ function suggestionWordsGenerator(wordsArray) {
 }
 
 function select () {
-    let allListItems = autoCompleteBox.querySelectorAll('li')
+    let allListItems = autoCompleteBox.querySelectorAll('li') //NodeList
     allListItems.forEach(function (wordItem) {
         wordItem.addEventListener('click', function (event) {
             searchInpueElem.value = event.target.textContent
@@ -47,3 +84,4 @@ function select () {
     })
 
 }
+*/
