b868829325d129eefcd09b155a7f606b0b17ba89
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-09-28 15:59:09 +0330

    js-exercise --> intermediate-Level -> 12-brightness-rangSlider: some refactor

--- pages/intermediate-level/12-brightness-rangeSlider-project/js/script.js
+++
@@ -1,9 +0,0 @@
-const bgImg = document.querySelector(".container")
-const inputRange = document.querySelector("#range")
-
-inputRange.addEventListener("change",(event) => {
-    console.log(event.target.value)
-    bgImg.style.filter = `brightness(${event.target.value}%)`
-
-})
-
