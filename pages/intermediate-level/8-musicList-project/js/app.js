4d4243657157c7b2e7958243c82b11632646b03b
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-10-13 11:42:21 +0330

    js-Exercise --> intermediate-level -> 8-Music-List : some refactor css & js Coding

--- pages/intermediate-level/8-musicList-project/js/app.js
+++ pages/intermediate-level/8-musicList-project/js/app.js
@@ -1,45 +1,33 @@
 let $ = document
-const players = $.querySelectorAll('.play-icon')
-const audios = $.querySelectorAll('audio')
+const players = $.querySelectorAll('.play-icon') //NodeList
+const audiosElm = $.querySelectorAll('audio')  //NodeList
 
 let musicName;
-let currentInon;
+let currentIcon;
 
-// Check if Playing
+// Flag: Check if Playing
 let isPlaying = false;
 
-players.forEach(function (player) {
+players.forEach((player) => {
 
-    player.addEventListener('click', function (event) {
+    player.addEventListener('click', (event) => {
+        currentIcon.nextElementSibling
         musicName = event.target.dataset.name
         currentIcon = event.target
 
-        audios.forEach(function (audio) {
+        audiosElm.forEach((audio) =>  {
             if (audio.dataset.name === musicName) {
                 if (audio.dataset.play === "false"){
-                    console.log(audio.dataset.play)
                     audio.currentTime = 0
-                    currentIcon.classList.replace("fa-pause", "fa-play");
-                    console.log(currentIcon.classList)
-                    audio.setAttribute("data-play", "true")
-                    // audio.dataset.play = "true"
-                    console.log(audio.dataset.play)
-                    audio.play()
-                } else {
                     currentIcon.classList.replace("fa-play", "fa-pause");
-                    audio.setAttribute("data-play", "false")
-                    // audio.dataset.play = "false"
-                    console.log(audio.dataset.play)
-                    console.log(currentIcon.classList)
+                    audio.setAttribute("data-play", "true")
+                    audio.play()
+
+                } else {
+                    currentIcon.classList.replace("fa-pause", "fa-play");
                     audio.pause()
+                    audio.setAttribute("data-play", "false")
                 }
-            } else {
-                currentIcon.classList.replace("fa-play", "fa-pause");
-                console.log(currentIcon.classList)
-                audio.setAttribute("data-play", "false")
-                // audio.dataset.play = "false"
-                console.log(audio.dataset.play)
-                audio.pause()
             }
         })
 
