ef26aaf7796a482f9a5cde126954a83015c3b58d
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-10-14 21:26:40 +0330

    js-Exercise --> js-intermediate -> 8-music-list : add styles width active class

--- pages/intermediate-level/8-musicList-project/js/app.js
+++ pages/intermediate-level/8-musicList-project/js/app.js
@@ -13,29 +13,36 @@ playericons.forEach((playericon) => {
     playericon.addEventListener('click', runMusic)
 })
 
-function runMusic(event){ 
-        currentIcon = event.target
-        musicIconName = event.target.dataset.name
-       
 
-        audiosElm.forEach((audio) => {
-            if (audio.dataset.name == musicIconName) {
-                previousAudio = audio
-                if (audio.dataset.play === "false") {
-                    currentIcon.classList.replace("fa-play", "fa-pause");
-                    audio.setAttribute("data-play", "true")
-                    audio.play()
+function runMusic(event) {
+    currentIcon = event.target
+    musicIconName = event.target.dataset.name
 
-                } else {
-                    currentIcon.classList.replace("fa-pause", "fa-play");
-                    audio.setAttribute("data-play", "false")
-                    audio.pause()
-                }
-            }else{
-                audio.pause()
-                audio.currentTime = 0
-                audio.previousElementSibling.classList.replace("fa-pause", "fa-play");
+    playericons.forEach((playericon) => {
+        playericon.classList.remove("active")
+    })
+    currentIcon.classList.add("active")
+
+    audiosElm.forEach((audio) => {
+        if (audio.dataset.name == musicIconName) {
+            previousAudio = audio
+            if (audio.dataset.play === "false") {
+                currentIcon.classList.replace("fa-play", "fa-pause");
+                audio.setAttribute("data-play", "true")
+                audio.play()
+
+            } else {
+                currentIcon.classList.replace("fa-pause", "fa-play");
                 audio.setAttribute("data-play", "false")
+                audio.pause()
             }
-        })
+        } else {
+            audio.pause()
+            audio.currentTime = 0
+            audio.previousElementSibling.classList.replace("fa-pause", "fa-play");
+            audio.setAttribute("data-play", "false")
+
+
+        }
+    })
 }
